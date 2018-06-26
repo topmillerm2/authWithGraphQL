c234395f19d820d2236dbadd5c50363cb0db82cc
Michael <michaeltopmiller@gmail.com>
2018-06-25 22:35:28 -0700

    added client side integration of mutations

--- client/index.js
+++ client/index.js
@@ -1,8 +1,36 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ApolloCLient, { createNetworkInterface } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 
-// const Root = () => {
-//   return <div>Auth Starter</div>;
-// };
+import App from './components/App';
+import LoginForm from './components/LoginForm';
+import SignupForm from './components/SignupForm';
 
-// ReactDOM.render(<Root />, document.querySelector('#root'));
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin'
+  }
+});
+
+const client = new ApolloCLient({
+  networkInterface,
+  dataIdFromObject: o => o.id
+});
+
+const Root = () => {
+  return (
+    <ApolloProvider client={client}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          <Route path="/login" component={LoginForm} />
+					<Route path="/signup" component={SignupForm} />
+        </Route>
+      </Router>
+    </ApolloProvider>
+  );
+};
+
+ReactDOM.render(<Root />, document.querySelector('#root'));
