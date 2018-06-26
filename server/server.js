dff154e1942b1f1a345891bdcfa815c5c7d8acd2
Michael <michaeltopmiller@gmail.com>
2018-06-25 21:02:53 -0700

    added login/logout/signup/currentuser mutations

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI =
+  'mongodb://topmillerm2:password123@ds217671.mlab.com:17671/graphauthqueryl';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
