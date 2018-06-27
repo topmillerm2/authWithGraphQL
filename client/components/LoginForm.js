895e5d847797c0f867159f055b6f932954a904ce
Michael <michaeltopmiller@gmail.com>
2018-06-26 21:25:25 -0700

    added a HOC and finalized auth flow

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -3,12 +3,20 @@ import AuthForm from './AuthForm';
 import mutation from '../mutations/Login';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { hashHistory } from 'react-router'
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.state = { errors: [] };
-  }
+	}
+	
+	componentWillUpdate(nextProps) {
+		if(!this.props.data.user && nextProps.data.user) {
+			hashHistory.push('/dashboard')
+		}
+	}
+	
   onSubmit({ email, password }) {
     this.props
       .mutate({
@@ -27,7 +35,7 @@ class LoginForm extends Component {
   render() {
     return (
       <div>
-        <h2>Login</h2>
+        <h3>Login</h3>
         <AuthForm
           errors={this.state.errors}
           onSubmit={this.onSubmit.bind(this)}
@@ -37,4 +45,4 @@ class LoginForm extends Component {
   }
 }
 
-export default graphql(mutation)(LoginForm);
+export default graphql(mutation)(graphql(query)(LoginForm));
